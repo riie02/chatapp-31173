787024c2eb6b7312cc5b1e034b8c678b027f99df
riie02 <riie0220@icloud.com>
2020-11-26 04:03:36 +0900

    チャット機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def new
+    @messages = Message.all
+    @message = Message.new
+    #@user = User.find
+  end
+
+  def create
+    @user = current_user
+    @message = Message.new(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message, user: @user
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id)
+  end
 end
---
+++ app/javascript/channels/message_channel.js
@@ -0,0 +1,20 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("MessageChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    const html = `<p><span>【</span>${data.user.name}<span>】</span>${data.content.updated_at}</p>
+                  <p>${data.content.text}</p>`;
+    const messages = document.getElementById('messages');
+    const newMessage = document.getElementById('message_text');
+    messages.insertAdjacentHTML('afterbegin', html);
+    newMessage.value='';
+  }
+});
